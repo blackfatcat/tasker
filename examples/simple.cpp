5643f24bbfc30c98c8606fecaf270be146e83281
blackfatcat <angel40.georgiev@gmail.com>
2026-04-19 09:28:58 +0200

    [CHANGED] example uses free functions for simplicity

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -9,51 +9,43 @@ struct Shutdown {};
 
 using tskr::Parallel;
 
-struct Task1
+void task3(const std::vector<int>& in)
 {
-    void operator()(const std::vector<int>& in) const
+    int total = 0;
+    for (const auto& i : in)
     {
-        int total = 0;
-        for (const auto& i : in)
-        {
-            total += i;
-        }
-        std::cout << "Total: " << total << std::endl;
+        total += i;
     }
-};
+    std::cout << "Total: " << total << std::endl;
+}
 
-struct Task2
-{
-    void operator()(const std::vector<int>& in) const
-    {
-        int total = 0;
-        for (const auto& i : in)
-        {
-            total += i;
-        }
-        std::cout << "Total: " << total << std::endl;
-    }
-};
 
-struct Task3
+void task2(const std::vector<int>& in)
 {
-    void operator()(const std::vector<int>& in) const
+    int total = 0;
+    for (const auto& i : in)
     {
-        int total = 0;
-        for (const auto& i : in)
-        {
-            total += i;
-        }
-        std::cout << "Total: " << total << std::endl;
+        total += i;
     }
-};
+    std::cout << "Total: " << total << std::endl;
+}
+
+void task1(const std::vector<int>& in)
+{
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
+    std::cout << "Total: " << total << std::endl;
+}
 
 int main() 
 {
     tskr::Tasker tasker;
 
     tasker.add_schedules<Startup, Parallel<Main, Render>, Shutdown>();
-    tasker.add_tasks<Startup>((tskr::TaskFn<Task1{}>{}, tskr::TaskFn<Task2{}>{}).after(tskr::TaskFn<Task3{}>{}));
+    tasker.add_tasks<Startup>((tskr::TaskFn<task1>{}, tskr::TaskFn<task2>{}).after(tskr::TaskFn<task3>{}));
 
     std::cout << "Scheduled" << std::endl;
 
