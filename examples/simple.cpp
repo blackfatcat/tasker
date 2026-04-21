1aca82c10636202ad506bcef4e9b65b0bdeb27b3
blackfatcat <angel40.georgiev@gmail.com>
2026-04-21 22:28:05 +0200

    [CHANGED] Simple test with atomic bools to make sure that schedule order and execution is correct

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -10,16 +10,25 @@ struct Shutdown {};
 
 using tskr::Parallel;
 
+static std::atomic_bool done1{ false };
+static std::atomic_bool done2{ false };
+static std::atomic_bool done3{ false };
+static std::atomic_bool done4{ false };
+static std::atomic_bool done5{ false };
+static std::atomic_bool done6{ false };
+static std::atomic_bool done7{ false };
+
 void task3()
 {
+    assert(done1.load(std::memory_order_acquire) && done2.load(std::memory_order_acquire));
     const std::vector<int>& in{ 1,2,3,4,5,6 };
     int total = 0;
     for (const auto& i : in)
     {
         total += i;
     }
+    done3.store(true, std::memory_order_release);
 }
-
 
 void task2()
 {
@@ -29,6 +38,8 @@ void task2()
     {
         total += i;
     }
+
+    done2.store(true, std::memory_order_release);
 }
 
 void task1()
@@ -39,24 +50,71 @@ void task1()
     {
         total += i;
     }
+
+    done1.store(true, std::memory_order_release);
+}
+
+void task4()
+{
+    assert(done1.load(std::memory_order_acquire) && done2.load(std::memory_order_acquire));
+    const std::vector<int>& in{ 1,2,3,4,5,6 };
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
+    done4.store(true, std::memory_order_release);
+}
+
+void task5()
+{
+    assert(done1.load(std::memory_order_acquire) && done2.load(std::memory_order_acquire));
+    const std::vector<int>& in{ 1,2,3,4,5,6 };
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
+    done5.store(true, std::memory_order_release);
+}
+
+void task6()
+{
+    assert(done1.load(std::memory_order_acquire) && done2.load(std::memory_order_acquire));
+    const std::vector<int>& in{ 1,2,3,4,5,6 };
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
+    done6.store(true, std::memory_order_release);
+}
+
+void task7()
+{
+    assert(done3.load(std::memory_order_acquire) && done4.load(std::memory_order_acquire));
+    assert(done5.load(std::memory_order_acquire) && done6.load(std::memory_order_acquire));
+
+    const std::vector<int>& in{ 1,2,3,4,5,6 };
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
 }
 
 int main() 
 {
-    //std::cout << "Task 1: " << typeid(&task1).name() << std::endl << "Task 2: " << typeid(&task2).name() << std::endl << "Task 3: " << typeid(&task3).name() << std::endl;
-
     tskr::Tasker tasker;
 
-    tasker.add_schedules<Startup>(tskr::ExecutionPolicy::Single);
-    tasker.add_schedules<Parallel<Main, Render>>(tskr::ExecutionPolicy::Repeat);
-    tasker.add_schedules<Shutdown>(tskr::ExecutionPolicy::Single);
+    tasker.add_schedules<Startup, Parallel<Main, Render>, Shutdown>(tskr::ExecutionPolicy::Single);
 
     tasker.add_tasks<Startup>((tskr::TaskFn<task1>{}, tskr::TaskFn<task2>{}));
 
-    tasker.add_tasks<Main>(tskr::TaskFn<task2>{}.after(tskr::TaskFn<task1>{}));
-    tasker.add_tasks<Render>(tskr::TaskFn<task2>{}.after(tskr::TaskFn<task1>{}));
+    tasker.add_tasks<Main>(tskr::TaskFn<task4>{}.after(tskr::TaskFn<task3>{}));
+    tasker.add_tasks<Render>(tskr::TaskFn<task6>{}.after(tskr::TaskFn<task5>{}));
 
-    tasker.add_tasks<Shutdown>(tskr::TaskFn<task3>{});
+    tasker.add_tasks<Shutdown>(tskr::TaskFn<task7>{});
 
     tasker.run();
 
