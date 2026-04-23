46d8a6231f3064380abc38aac0d17b6b1b84f1de
blackfatcat <angel40.georgiev@gmail.com>
2026-04-23 13:25:41 +0200

    [CHANGED] extended to test before() and greater task chaining

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -17,24 +17,26 @@ static std::atomic_bool done4{ false };
 static std::atomic_bool done5{ false };
 static std::atomic_bool done6{ false };
 static std::atomic_bool done7{ false };
+static std::atomic_bool done8{ false };
+static std::atomic_bool done9{ false };
+static std::atomic_bool done10{ false };
 
 struct VecRes
 {
     std::vector<int> vec{ 1,2,3,4,5,6 };
 };
 
-void task3(tskr::Resource<VecRes> vec_res)
+void task1()
 {
-    assert(done1.load(std::memory_order_acquire) && done2.load(std::memory_order_acquire));
-
-    vec_res->vec.push_back(7);
     const std::vector<int>& in{ 1,2,3,4,5,6 };
     int total = 0;
     for (const auto& i : in)
     {
         total += i;
     }
-    done3.store(true, std::memory_order_release);
+    std::cout << "Start" << std::endl;
+
+    done1.store(true, std::memory_order_release);
 }
 
 void task2()
@@ -49,16 +51,18 @@ void task2()
     done2.store(true, std::memory_order_release);
 }
 
-void task1()
+void task3(tskr::Resource<VecRes> vec_res)
 {
+    assert(done1.load(std::memory_order_acquire) && done2.load(std::memory_order_acquire));
+
+    vec_res->vec.push_back(7);
     const std::vector<int>& in{ 1,2,3,4,5,6 };
     int total = 0;
     for (const auto& i : in)
     {
         total += i;
     }
-
-    done1.store(true, std::memory_order_release);
+    done3.store(true, std::memory_order_release);
 }
 
 void task4(tskr::Resource<VecRes> vec_res)
@@ -97,17 +101,12 @@ void task6()
 
 void task7(
     tskr::Resource<tskr::Repeating<Main>> main_schedule_repeating
-    // tskr::Resource<tskr::Repeating<Startup>> startup_schedule_repeating
 )
 {
-    assert(done3.load(std::memory_order_acquire) && done4.load(std::memory_order_acquire));
-    assert(done5.load(std::memory_order_acquire) && done6.load(std::memory_order_acquire));
+    assert(done1.load(std::memory_order_acquire) && done2.load(std::memory_order_acquire));
 
     // Stops Main and Render from repeating, terminating the program
     main_schedule_repeating->stop();
-
-    // Starts repeating Startup
-    // startup_schedule_repeating->start();
 
     const std::vector<int>& in{ 1,2,3,4,5,6 };
     int total = 0;
@@ -115,6 +114,60 @@ void task7(
     {
         total += i;
     }
+    done7.store(true, std::memory_order_release);
+}
+
+void task8()
+{
+    assert(done5.load(std::memory_order_acquire) && done6.load(std::memory_order_acquire) && done7.load(std::memory_order_acquire));
+
+    const std::vector<int>& in{ 1,2,3,4,5,6 };
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
+    done8.store(true, std::memory_order_release);
+}
+
+void task9()
+{
+    assert(done5.load(std::memory_order_acquire) && done6.load(std::memory_order_acquire) && done7.load(std::memory_order_acquire));
+
+    const std::vector<int>& in{ 1,2,3,4,5,6 };
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
+    done9.store(true, std::memory_order_release);
+}
+
+void task10()
+{
+    assert(done5.load(std::memory_order_acquire) && done6.load(std::memory_order_acquire) && done7.load(std::memory_order_acquire));
+
+    const std::vector<int>& in{ 1,2,3,4,5,6 };
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
+    done10.store(true, std::memory_order_release);
+}
+
+void task11()
+{
+    assert(done8.load(std::memory_order_acquire) && done9.load(std::memory_order_acquire) && done10.load(std::memory_order_acquire));
+
+    const std::vector<int>& in{ 1,2,3,4,5,6 };
+    int total = 0;
+    for (const auto& i : in)
+    {
+        total += i;
+    }
+
+    std::cout << "Done" << std::endl;
 }
 
 int main() 
@@ -126,8 +179,8 @@ int main()
         .add_schedules<Shutdown>(tskr::ExecutionPolicy::Single)
         .add_tasks<Startup>((tskr::TaskFn<task1>{}, tskr::TaskFn<task2>{}))
         .add_tasks<Main>(tskr::TaskFn<task4>{}.after(tskr::TaskFn<task3>{}))
-        .add_tasks<Render>(tskr::TaskFn<task6>{}.after(tskr::TaskFn<task5>{}))
-        .add_tasks<Shutdown>(tskr::TaskFn<task7>{})
+        .add_tasks<Render>((tskr::TaskFn<task6>{}, tskr::TaskFn<task7>{}).after(tskr::TaskFn<task5>{}))
+        .add_tasks<Shutdown>((tskr::TaskFn<task8>{}, tskr::TaskFn<task9>{}, tskr::TaskFn<task10>{}).before(tskr::TaskFn<task11>{}))
         .register_resource(VecRes{})
         .run();
 
